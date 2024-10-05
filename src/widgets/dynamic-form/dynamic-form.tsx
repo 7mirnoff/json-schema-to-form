711ad89cbc5d85de76cbd796fea21297d1df4428
Dmitry Smirnov <dima7mirnoff@gmail.com>
2024-10-05 07:21:40 +0300

    implement scroll to error field

--- src/widgets/dynamic-form/dynamic-form.tsx
+++ src/widgets/dynamic-form/dynamic-form.tsx
@@ -13,11 +13,21 @@ export const DynamicForm: React.FC<DynamicFormProps> = ({ schema, onSubmit }) =>
     }
   }, [schema])
 
+  function onFormError() {
+    const firstErrorField = Object.keys(form.formState.errors)[0]
+    if (firstErrorField) {
+      const errorElement = document.querySelector(`[name="${firstErrorField}"]`)
+      if (errorElement) {
+        errorElement.scrollIntoView({ behavior: 'smooth', block: 'center' })
+      }
+    }
+  }
+
   return (
     <FormProvider {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} noValidate>
-        <SchemaField schema={schema} path='' title='dynamicForm' required={rootRequired} />
-        <Toolbar sx={{ justifyContent: "center" }}>
+      <form onSubmit={form.handleSubmit(onSubmit, onFormError)} noValidate>
+        <SchemaField schema={schema} path="" title="dynamicForm" required={rootRequired} />
+        <Toolbar sx={{ justifyContent: 'center' }}>
           <Button type="submit" variant="contained">Отправить</Button>
         </Toolbar>
       </form>
